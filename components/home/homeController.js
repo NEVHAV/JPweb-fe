6fe8cd6ff15e956d3f9a879b5796803ac36c31f5
Ly Trung Hieu <hieult@vnist.vn>
2017-12-27 14:52:50 +0700

    first commit

---
+++ components/home/homeController.js
@@ -0,0 +1,33 @@
+/**
+ * Created by NEVHAV on 26/12/17.
+ */
+angular.module('JPweb-fe')
+    .controller('homeController', function ($scope, $http, $state){
+        $scope.title = 'JPweb - Home';
+        $(document).ready(function(){
+            $('.carousel').carousel();
+        });
+
+        $scope.goMain = goMain;
+        function  goMain() {
+            $state.go('main');
+        }
+        $scope.user = {};
+        $scope.signIn = function () {
+            console.log('go');
+            var url = "http://localhost:8080/JPweb-be/logIn";
+            $http({
+                method: 'POST',
+                url: url,
+                data: $scope.user
+            }).then(function (response) {
+                console.log(response);
+                console.log('success');
+                goMain();
+            }, function (error) {
+                console.log(error);
+                console.log('error: method post');
+                Materialize.toast('Your username hasn\'t had or your password is wrong!', 4000);
+            })
+        }
+    });
---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+/**
+ * Created by NEVHAV on 11/11/17.
+ */
+angular.module('JPweb-fe', [
+    'oc.lazyLoad',
+    'ui.router'
+])
+    .constant('API_URL_VOCALB', 'http://localhost:8080/JPweb-be/')
+    .constant('API_URL_GRAMMAR', 'http://localhost:8080/JPweb-be/')
+    .constant('API_URL', '')
+    // .constant('API_URL', 'http://localhost:8080/contacts/public/contacts/')
+
+    .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('/home');
+        $stateProvider
+            .state('main', {
+                url: '/main',
+                templateUrl: 'components/main/mainView.html',
+                controller: 'mainController',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load(
+                            {
+                                files: ['components/main/mainController.js']
+                            }
+                        )
+                    }
+                }
+            })
+
+            .state('home', {
+                url: '/home',
+                templateUrl: 'components/home/homeView.html',
+                controller: 'homeController',
+                resolve: {
+                    loadMyFiles: function ($ocLazyLoad) {
+                        return $ocLazyLoad.load(
+                            {
+                                files: ['components/home/homeController.js']
+                            }
+                        )
+                    }
+                }
+            })
+    }]);
